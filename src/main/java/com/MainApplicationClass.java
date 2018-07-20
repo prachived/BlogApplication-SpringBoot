b9cccdeb229e67a83554bf182daca93331e3f8ef
Prachi Ved <prved@ccs.neu.edu>
2018-07-19 20:56:42 -0400

    Component Scan update

--- src/main/java/com/MainApplicationClass.java
+++ src/main/java/com/MainApplicationClass.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan({"com.controller","com.model","com.repository"})
 @EnableJpaRepositories("com.repository")
 public class MainApplicationClass {
 
